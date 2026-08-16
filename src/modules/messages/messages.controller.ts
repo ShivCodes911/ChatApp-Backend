3d9d20fd751cdf0916a636d024597c71611f2a63
ShivCodes911 <workforshiv09@gmail.com>
2026-08-16 23:36:10 +0530

    feat: add message deletion API

--- src/modules/messages/messages.controller.ts
+++ src/modules/messages/messages.controller.ts
@@ -87,6 +87,17 @@ export const getAllMessage=async(req:Request,res:Response,next:NextFunction)=>{
     try {
         const currentUserId=req.user?.userId;
 
+        const {page,limit}=req.query;
+
+        const pageNumber =Number(page) || 1;
+        const limitNumber =Number(limit) || 20;
+
+        const skip=(pageNumber-1)*limitNumber;
+
+
+
+
+
         if(!currentUserId){
             return res.status(400).json({
                 status:false,
@@ -119,7 +130,7 @@ export const getAllMessage=async(req:Request,res:Response,next:NextFunction)=>{
 
     const messages = await messageModel.find({
     conversation: conversationId
-}).sort({createdAt:1});
+}).populate("sender","name email").sort({createdAt:1}).skip(skip).limit(limitNumber);
 
 // 5. Check if conversation has no messages
         if (messages.length === 0) {
@@ -131,9 +142,80 @@ export const getAllMessage=async(req:Request,res:Response,next:NextFunction)=>{
         return res.status(200).json({
             status:true,
             message:"Messages fetched successfully",
-            data:messages
+            data:messages,
+            pagination:{
+                page:pageNumber,
+                limit:limitNumber
+            }
         })
     } catch (error) {
+        next(error);
+        
+    }
+};
+
+export const deleteMessage=async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const currentUserId=req.user?.userId;
+
+        if(!currentUserId){
+            return res.status(400).json({
+                status:false,
+                message:"Unauthorized"
+            })
+        }
+
+        const {conversationId,messageId}=req.params;
+
+        if (!conversationId ||Array.isArray(conversationId) ||!messageId ||Array.isArray(messageId)) {
+                return res.status(400).json({
+        status: false,
+        message: "Invalid conversation ID or message ID"
+        });
+    }
+
+
+            const conversation = await ConversationModel.findOne({
+                    _id: conversationId,
+                participants: currentUserId
+                    });
+
+            if(!conversation){
+                return res.status(404).json({
+                    status:false,
+                    messsage:"Conversation of this not found"
+                })
+            };
+
+
+            const message=await messageModel.findOne({_id:messageId});
+
+            if(!message){
+                return res.status(404).json({
+                    status:false,
+                    message:"Message not found"
+                })
+            };
+
+            if(message.sender.toString()!==currentUserId){
+               return res.status(403).json({
+        status: false,
+        message: "You can only delete your own messages"
+    });
+            };
+
+
+        await messageModel.deleteOne({
+            _id:messageId,
+            sender:currentUserId
+        })
+
+            return res.status(200).json({
+                status:true,
+                message:"message deleted successfully"
+            })
+
+ } catch (error) {
         next(error);
         
     }
--- src/modules/messages/messages.routes.ts
+++ src/modules/messages/messages.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 import { userAuthMiddleware } from "../../middlewares/auth.middleware.js";
-import { createMessage, getAllMessage } from "./messages.controller.js";
+import { createMessage, deleteMessage, getAllMessage } from "./messages.controller.js";
 
 const router = express.Router();
 
 router.post("/conversations/:conversationId/messages",userAuthMiddleware,createMessage);
 router.get("/conversations/:conversationId/messages",userAuthMiddleware,getAllMessage);
+router.delete("/conversations/:conversationId/messages/:messageId",userAuthMiddleware,deleteMessage);
 
 export default router;
